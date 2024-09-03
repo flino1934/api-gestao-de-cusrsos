9befb1af0a7a3f1f3186d6c29cb524dbb06e2a60
FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>
2024-09-03 16:20:23 -0300

    Enrollment, EnrolmentPk
    Signed-off-by: FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>

---
+++ src/main/java/com/lino/dslearnbds/entities/Enrrollment.java
@@ -0,0 +1,95 @@
+package com.lino.dslearnbds.entities;
+
+import com.lino.dslearnbds.entities.pk.EnrrolmentPK;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_enrollment")
+public class Enrrollment {//Responsavel pela matricula
+
+    @EmbeddedId//Pois vai usar um id composto que vem de offer e user
+    private EnrrolmentPK id = new EnrrolmentPK();
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant enrollMoment;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant refundMoment;
+    private boolean available;
+    private boolean onlyUpdate;//para saber se ele pode receber uma atualização do curso
+
+    public Enrrollment() {
+    }
+
+    public Enrrollment(User user,Offer offer, Instant enrollMoment, Instant refundMoment, boolean available, boolean onlyUpdate) {
+        id.setUser(user);//Esta passando como id o objeto user
+        id.setOffer(offer);//Esta passando como id o objeto Offer
+        this.enrollMoment = enrollMoment;
+        this.refundMoment = refundMoment;
+        this.available = available;
+        this.onlyUpdate = onlyUpdate;
+    }
+
+
+    public User getStudent(){//Vai pegar o usuario
+        return id.getUser();
+    }
+
+    public void setStudent(User user){
+        id.setUser(user);
+    }
+
+    public Offer getOffer(){
+        return id.getOffer();
+    }
+
+    public void setOffer(Offer offer){
+        id.setOffer(offer);
+    }
+
+    public Instant getEnrollMoment() {
+        return enrollMoment;
+    }
+
+    public void setEnrollMoment(Instant enrollMoment) {
+        this.enrollMoment = enrollMoment;
+    }
+
+    public Instant getRefundMoment() {
+        return refundMoment;
+    }
+
+    public void setRefundMoment(Instant refundMoment) {
+        this.refundMoment = refundMoment;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public boolean isOnlyUpdate() {
+        return onlyUpdate;
+    }
+
+    public void setOnlyUpdate(boolean onlyUpdate) {
+        this.onlyUpdate = onlyUpdate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Enrrollment that = (Enrrollment) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/lino/dslearnbds/entities/pk/EnrrolmentPK.java
@@ -0,0 +1,58 @@
+package com.lino.dslearnbds.entities.pk;
+
+import com.lino.dslearnbds.entities.Offer;
+import com.lino.dslearnbds.entities.User;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class EnrrolmentPK implements Serializable {//Essa classe vai conter o id de user e oferta para saber qual usuario tem qual oferta e qual oferta tem qual usuario
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "offer_id")
+    private Offer offer;
+
+    public EnrrolmentPK() {
+    }
+
+    public EnrrolmentPK(User user, Offer offer) {
+        this.user = user;
+        this.offer = offer;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Offer getOffer() {
+        return offer;
+    }
+
+    public void setOffer(Offer offer) {
+        this.offer = offer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EnrrolmentPK that = (EnrrolmentPK) o;
+        return Objects.equals(user, that.user) && Objects.equals(offer, that.offer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, offer);
+    }
+}
